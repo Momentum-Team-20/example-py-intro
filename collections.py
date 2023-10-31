57d37e1bdbc5190f6b5cbf262e64f5537a6e6f54
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-10-31 11:30:55 -0400

    up to dictionary example

---
+++ collections.py
@@ -0,0 +1,7 @@
+['The Bear', 'Witch', 'Cow', 'Mr. Clean', 'Cheshire Cat', 'Bumblebee', 'Dinosaur']
+Dinosaur
+['The Bear', 'Witch', 'Cow', 'Mr. Clean', 'Cheshire Cat', 'Bumblebee']
+6
+['Cow', 'Mr. Clean', 'Cheshire Cat']
+The costumes we are keeping for next year are:  ['Witch', 'Cow', 'Mr. Clean', 'Cheshire Cat', 'Bumblebee']
+('red', 'blue', 'green', 'yellow')
